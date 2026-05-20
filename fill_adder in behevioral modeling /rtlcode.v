01cd72f7585ba89cd2dcc6e5127e3a982fcac7dd
Vaishnavi8504 <vaishnavipathak04@gmail.com>
2026-05-20 13:23:21 +0530

    Create rtlcode.v

---
+++ fill_adder in behevioral modeling /rtlcode.v
@@ -0,0 +1,7 @@
+module full_adder(input a_fa,b_fa,cin_fa, output  reg sum_fa, output reg carry_fa);
+always @(*)
+begin 
+sum_fa = a_fa^b_fa^cin_fa;
+carry_fa =( a_fa & b_fa)|(b_fa & cin_fa)|(cin_fa & a_fa);
+end
+endmodule

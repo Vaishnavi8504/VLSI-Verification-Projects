e04cf261a2b79c6edffea371a1632995cec1d7cf
Vaishnavi8504 <vaishnavipathak04@gmail.com>
2026-05-20 15:34:08 +0530

    Create testbench.v

---
+++ full_adder_inbehavioralmodeling/testbench.v
@@ -0,0 +1,30 @@
+module full_adder_tb( );
+//step 1declare the variable 
+reg a_tb,b_tb,cin_tb;
+wire sum_tb,carry_tb;
+//step 2 instantiation 
+full_adder dut (a_tb,b_tb,cin_tb,sum_tb,carry_tb);
+//step 3 initialization of i/p and o/p
+initial 
+begin
+a_tb=0;
+b_tb=0;
+cin_tb=0;
+end 
+//step 4 driving the i/p &o/p
+initial
+begin 
+$monitor ("a_tb=%b,b_tb=%b,cin_tb=%b, sum_tb=%b,carry_tb =%b", a_tb,b_tb,cin_tb ,sum_tb,carry_tb);
+a_tb=1'b1;
+b_tb=1'b1;
+cin_tb=1'b1;
+#1;
+a_tb=1'b0;
+b_tb=1'b0;
+cin_tb=1'b1;
+#1;
+a_tb=1'b1;
+b_tb=1'b1;
+cin_tb=1'b0;
+end
+endmodule

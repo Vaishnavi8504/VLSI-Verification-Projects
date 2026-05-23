9c5af84641f6dbdf9a492cc0909e41a791bd82f1
Vaishnavi8504 <vaishnavipathak04@gmail.com>
2026-05-23 10:29:55 +0530

    Create rtlcode.v

---
+++ mux_4_1/rtlcode.v
@@ -0,0 +1,20 @@
+module mux_4_1( input [3:0] i,input [1:0]s,output reg y);
+always @(*)
+begin
+case (s)
+2'b00:y=i[0];
+2'b01:y=i[1];
+2'b10:y=i[2];
+2'b11:y=i[3];
+  //if else 
+  // if (s==2'b00)
+  //   y=i[0];
+  // else if (s==2'b01)
+  //   y=i[1];
+  // else if (s==2'b10)
+  //   y=i[2];
+  // else if (s==2'b11)
+  //   y=i[3];
+endcase
+end
+endmodule

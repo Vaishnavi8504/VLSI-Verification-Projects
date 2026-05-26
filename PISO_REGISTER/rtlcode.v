98e225824f7dc9861ca7eedd301f1ad431be1c3e
Vaishnavi8504 <vaishnavipathak04@gmail.com>
2026-05-26 21:00:11 +0530

    Create rtlcode.v

---
+++ PISO_REGISTER/rtlcode.v
@@ -0,0 +1,19 @@
+module piso_register( input clk ,rst,load,input  [3:0]p_in,output reg s_out);
+ reg [3:0] temp;
+ always @(posedge clk)
+ begin
+ if (rst)
+ temp<= 4'b0000;
+ else if (load)
+ begin
+ temp<=p_in;
+ end
+ else if (load==0)
+ begin
+  temp<= {1'b0,temp[3:1]};
+  s_out<=temp[0];
+ end
+ 
+ end
+ 
+endmodule

3c271d1742acb2eda6a05fd42a8c1a3aebf8e440
Vaishnavi8504 <vaishnavipathak04@gmail.com>
2026-05-26 21:19:16 +0530

    Create rtlcode.v

---
+++ PIPO_REGISTER/rtlcode.v
@@ -0,0 +1,12 @@
+odule pipo_register(input clk,rst,load,input [3:0]pin ,output reg [3:0] pout );
+reg [3:0]temp;
+always @(posedge clk)
+begin
+if (rst)
+temp=4'b0000;
+else if (load)
+temp<=pin;
+else if(load==0)
+pout<=temp;
+end
+endmodule

9b908f71f6314e30855d5d8d5a636e442c16b219
Vaishnavi8504 <vaishnavipathak04@gmail.com>
2026-06-02 16:55:11 +0530

    Update rtlcode.v

--- universal_asynchronous_even_counter/rtlcode.v
+++ universal_asynchronous_even_counter/rtlcode.v
@@ -1,14 +1,19 @@
-module universal_counter(input clk,rst,enb, input [1:0]mode,output reg mod_2counter,
+module universal_counter(input clk,rst,enb ,up_downbar , input [1:0]mode,output reg mod_2counter,
 output reg [1:0] mod_4counter,
 output reg[2:0] mod_8counter,
 output reg [3:0]mod_16counter);
 reg [3:0] internal_counter;
+   //in these code we can design up and down counter also by declaring up_downbar 
+   // and for 0 it will down counter and for 1  it will be up counter 
 always @(posedge clk)
 begin
 if(rst)
    internal_counter<=0;
-  else if(enb)
+   else if(enb && up_downbar)
   internal_counter<= internal_counter+1'b1;
+   else if(enb && up_downbar==0)
+  internal_counter<= internal_counter-1'b1;
+   
   else 
   internal_counter=internal_counter;
   end
